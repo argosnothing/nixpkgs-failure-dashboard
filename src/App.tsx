b964739310a1d5c68a76b4578917df22bc61403e
Sigmanificient <edhyjox@gmail.com>
2026-03-28 23:57:52 +0100

    Scroll to selected element

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
-import { List, type RowComponentProps } from "react-window";
+import { useEffect, useState, type RefObject } from "react";
+import { List, useListRef, type ListImperativeAPI, type RowComponentProps } from "react-window";
 
 import "./App.scss";
+import { useSearchParams } from "react-router-dom";
 
 interface Build {
   id: number;
@@ -30,9 +31,13 @@ function BuildEntry({
   );
 }
 
-function BuildsTable({ builds }: { builds: Build[]; }) {
+function BuildsTable({ builds, listRef }: {
+  builds: Build[];
+  listRef: RefObject<ListImperativeAPI | null> }
+) {
     return (
       <List
+        listRef={listRef}
         className="builds-table"
         rowComponent={BuildEntry}
         rowCount={builds.length}
@@ -43,28 +48,38 @@ function BuildsTable({ builds }: { builds: Build[]; }) {
 }
 
 export default function App() {
-  // const [searchParams, _] = useSearchParams();
-  // console.log(searchParams);
+  const [searchParams, _] = useSearchParams();
+  let selected = searchParams.get("selected");
 
   const [data, setData] = useState<Build[]>([]);
+  const listRef = useListRef(null);
 
   useEffect(() => {
     fetch(`/api/builds`)
       .then((res) => res.json())
       .then((res: ApiResponse) => {
         setData(res);
+
+        if (selected == null) return;
+        let idx = res.findIndex((b => b.attrpath == selected), res);
+        if (idx == -1) return;
+
+        const list = listRef.current;
+        list?.scrollToRow({
+          align: "start",
+          behavior: "smooth",
+          index: idx
+        });
       });
   }, []);
 
   if (data.length == 0)
     return <p>Loading...</p>;
 
-  console.log(data.length);
-
   return (
     <>
       <h2>Builds ({data.length})</h2>
-      <BuildsTable builds={data} />
+      <BuildsTable listRef={listRef} builds={data} />
     </>
   );
 }
