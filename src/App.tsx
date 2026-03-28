2c4e0ccd9f214e428822feae086299b9998084ae
Sigmanificient <edhyjox@gmail.com>
2026-03-29 00:29:55 +0100

    Fix scrolling in release mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState, type RefObject } from "react";
-import { List, useListRef, type ListImperativeAPI, type RowComponentProps } from "react-window";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { List, useListRef, type RowComponentProps } from "react-window";
 
 import "./App.scss";
 import { useSearchParams } from "react-router-dom";
@@ -31,20 +31,40 @@ function BuildEntry({
   );
 }
 
-function BuildsTable({ builds, listRef }: {
+function BuildsTable({ builds, top }: {
   builds: Build[];
-  listRef: RefObject<ListImperativeAPI | null> }
-) {
-    return (
-      <List
-        listRef={listRef}
-        className="builds-table"
-        rowComponent={BuildEntry}
-        rowCount={builds.length}
-        rowHeight={25}
-        rowProps={{ builds }}
-      />
-    )
+  top: string | null;
+}) {
+  const listRef = useListRef(null);
+
+  useLayoutEffect(() => {
+    if (!top) return;
+
+    const idx = builds.findIndex((b) => b.attrpath === top);
+    if (idx === -1) return;
+
+    requestAnimationFrame(() => {
+      const list = listRef.current;
+
+      list?.scrollToRow({
+        align: "start",
+        behavior: "smooth",
+        index: idx,
+      });
+    });
+  }, []);
+
+
+  return (
+    <List
+      listRef={listRef}
+      className="builds-table"
+      rowComponent={BuildEntry}
+      rowCount={builds.length}
+      rowHeight={25}
+      rowProps={{ builds }}
+    />
+  );
 }
 
 export default function App() {
@@ -52,24 +72,12 @@ export default function App() {
   let selected = searchParams.get("selected");
 
   const [data, setData] = useState<Build[]>([]);
-  const listRef = useListRef(null);
 
   useEffect(() => {
     fetch(`/api/builds`)
       .then((res) => res.json())
       .then((res: ApiResponse) => {
         setData(res);
-
-        if (selected == null) return;
-        let idx = res.findIndex((b => b.attrpath == selected), res);
-        if (idx == -1) return;
-
-        const list = listRef.current;
-        list?.scrollToRow({
-          align: "start",
-          behavior: "smooth",
-          index: idx
-        });
       });
   }, []);
 
@@ -79,7 +87,7 @@ export default function App() {
   return (
     <>
       <h2>Builds ({data.length})</h2>
-      <BuildsTable listRef={listRef} builds={data} />
+      <BuildsTable top={selected} builds={data} />
     </>
   );
 }
